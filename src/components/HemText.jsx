909ca935efd92b061dbcd308039e788b94864437
Fredrik <fredrik.lastname@gmail.com>
2021-06-03 05:14:29 +0200

    hem-sidan funktionsduglig

---
+++ src/components/HemText.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function HemText(){
+    return(
+        <div className="hemText">
+            <div className="hemText-content">
+                
+                <span>
+                    Hej!<br/>
+                    Här är din shoppinglista. Glöm inte att kolla om det kommit in några tips på erbjudanden som du kan vara intresserad av!
+                </span>                    
+
+            </div>
+        </div>
+    )
+}
+
+export default HemText;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
 //Sida som är tänkt att utgöra ramen för innehållet
 import React from "react"
 import ShoppingList from "./ShoppingList"
+import HemText from "./HemText"
 function Home(){
 
     return(
         <div>
-            <div className ="wrapper__content">
-                
+            <div className ="wrapper__content_home">
+                <HemText />
                 <ShoppingList />
             </div>   
         </div>
